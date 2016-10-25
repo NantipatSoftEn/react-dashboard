f5576f62263ca0d8c12cc1eb64259dae77c67b06
Teerapong Singthong <st.teerapong@gmail.com>
2016-10-25 16:28:57 +0700

    Re-design product component page

--- src/app/containers/pages/Product.js
+++ src/app/containers/pages/Product.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
-import render from 'react-dom'
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
 
-import Header from '../../components/product/Header'
-import Items from '../../components/product/Items'
-import ShowAvatar from '../../components/product/ShowAvatar'
+import Item from '../../components/product/Item'
 
 class Product extends Component {
+	constructor (props, context) {
+		super(props, context);
+	}
+
 	render () {
 		return (
-			<div>
-				<Header
-					title="Add item to cart">
-				</Header>
-				<ShowAvatar></ShowAvatar>
-				<Items></Items>
+			<div className="row">
+				<Item productTitle="Lorem Ipsum" />
+				<Item productTitle="Richard McClintock" />
+				<Item productTitle="H. Rackham" />
+				<Item productTitle="Letraset sheets containing" />
 			</div>
 		)
 	}
