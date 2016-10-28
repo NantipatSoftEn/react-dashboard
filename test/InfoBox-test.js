507413fedecee4ed09afa03951db36b1570e9bbb
Teerapong Singthong <st.teerapong@gmail.com>
2016-10-28 11:15:56 +0700

    Add mocha enzyme spec for component testing

--- test/InfoBox-test.js
+++ test/InfoBox-test.js
@@ -5,15 +5,17 @@ import { shallow, mount, render } from 'enzyme';
 import InfoBox from '../src/app/components/widget/InfoBox';
 
 describe("InfoBox - suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(mount(<InfoBox />).find('.info-box').length).to.equal(1);
+    const wrapper = shallow(<InfoBox />);
+
+    it('should be renders one <InfoBox /> component', () => {
+        expect(wrapper).to.have.length(1);
     });
 
-    // it("contains spec with an expectation", function() {
-    //     expect(shallow(<Foo />).is('.foo')).to.equal(true);
-    // });
-    //
-    // it("contains spec with an expectation", function() {
-    //     expect(mount(<Foo />).find('.foo').length).to.equal(1);
-    // });
+    it('shoud be contains only one <className="info-box" />', () => {
+        expect(wrapper.find('.info-box').length).to.equal(1);
+    });
+
+    it('should be contains only one <className="info-box-content">', () => {
+        expect(wrapper.find('.info-box-content')).to.have.length(1);
+    });
 });
