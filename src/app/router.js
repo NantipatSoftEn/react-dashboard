40a15708b974bc58849d47e16b8186b5448d44c1
Teerapong Singthong <st.teerapong@gmail.com>
2016-10-31 14:25:18 +0700

    Add nested /product/detail/:productID router

--- src/app/router.js
+++ src/app/router.js
@@ -8,6 +8,7 @@ import App from './containers/layout/App';
 import Home from './containers/pages/Home';
 import Todo from './containers/pages/Todo';
 import Product from './containers/pages/Product';
+import ProductDetail from './containers/pages/ProductDetail';
 import EasyRedux from './containers/pages/EasyRedux';
 import ReactLifeCycle from './containers/pages/ReactLifeCycle';
 
@@ -23,16 +24,21 @@ export default (
 				pageDesc="This is a react sandbox" />
 			<Route path='home'
 				component={Home}
-				title="Home | My Sandbox" />
+				title="Home | My Sandbox"
+				pageDesc="Dashboard" />
 			<Route path='todo'
 				component={Todo}
 				title="Todo | Pull real-time data from database" />
 			<Route path='product'
 				component={Product}
 				title="Product | Add product to cart" />
+			<Route path='/product/detail/:productID'
+				component={ProductDetail}
+				title="Product Detail" />
 			<Route path='dashboard/chart'
 				component={Chart}
-				title="Chart | Chart summary" />
+				title="Chart | Chart summary"
+				pageDesc="Summary data" />
 			<Route path='easy-redux'
 				component={EasyRedux}
 				title="Easy Redux | To learn easy redux from simple file" />
